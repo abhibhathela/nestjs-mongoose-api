39cd1d48cc3354f44e23697587ee02df620b18bd
Abhishek <abhibhathela@gmail.com>
2023-04-04 22:33:21 +0530

    added validation

--- src/books/dto/create-book.dto.ts
+++ src/books/dto/create-book.dto.ts
@@ -1,5 +1,13 @@
+import { IsNotEmpty, Max, Min } from 'class-validator';
+
 export class CreateBookDto {
+  @IsNotEmpty()
   readonly title: string;
+
+  @IsNotEmpty()
   readonly author: string;
+
+  @Min(1900)
+  @Max(new Date().getFullYear())
   readonly year: number;
 }
